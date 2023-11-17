e9a94b93a0edfef8279b1c038babfe3a9d85e178
Leo Mikko Uson <lancerthortan@gmail.com>
2023-11-17 22:43:17 +0800

    Delete dashboard and header files

---
+++ admin/dashboard/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <h3>Event</h3>
+            <ul>
+                <li><a href="#/">Overview</a></li>
+                <li><a href="rso.php">RSO</a></li>
+                <li><a href="#/admin">Admin</a></li>
+                <li><a href="#/reports">Reports</a></li>
+            </ul>
+        </div>
+    </nav>
+
+</body>
+
+</html>
