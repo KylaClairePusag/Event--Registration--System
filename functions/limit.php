2817d7a545874ce83ab93fdb118879432b84ea49
Leo Mikko Uson <lancerthortan@gmail.com>
2023-11-18 13:54:40 +0800

    Refactor limit.php and table.component.php

--- functions/limit.php
+++ functions/limit.php
@@ -1,16 +1,18 @@
 <?php
-// Assume $searchQuery is passed to this file before including it
 $searchQuery = $_GET['limit'] ?? '';
 ?>
 
-<label for="limit"></label>
+<label for="limit">
+
+</label>
 <select id="limit" name="limit" onchange="changeLimit(this.value)">
     <option value="5" <?php if ($limit == 5)
-        echo 'selected'; ?>>Limit: 5</option>
+        echo 'selected'; ?>>Limit: 5 </option>
     <option value="10" <?php if ($limit == 10)
         echo 'selected'; ?>>Limit: 10</option>
     <option value="15" <?php if ($limit == 15)
         echo 'selected'; ?>>Limit: 15</option>
     <option value="20" <?php if ($limit == 20)
         echo 'selected'; ?>>Limit: 20</option>
+
 </select>
--- functions/table.component.php
+++ functions/table.component.php
@@ -1,23 +1,39 @@
 <?php
 function createTable($head, $body)
 {
+    if (empty($body)) {
+        echo '<p>No data available.</p>';
+        return;
+    }
+
+    // Check if the password column is present in the $head array
+    $hasPasswordColumn = in_array('Password', $head);
+
     echo '<table>';
     echo '<thead>';
     echo '<tr>';
     foreach ($head as $column) {
-        echo '<th>' . $column . '</th>';
+        // Only display the column if it is not the password column
+        if (!$hasPasswordColumn || (strtolower($column) !== 'password')) {
+            echo '<th>' . $column . '</th>';
+        }
     }
     echo '</tr>';
     echo '</thead>';
     echo '<tbody>';
     foreach ($body as $row) {
         echo '<tr>';
-        foreach ($row as $cell) {
-            echo '<td>' . $cell . '</td>';
+        foreach ($row as $key => $cell) {
+            // Only display the cell if it corresponds to a non-password column
+            if (!$hasPasswordColumn || (strtolower($head[$key]) !== 'password')) {
+                echo '<td>' . $cell . '</td>';
+            }
         }
         echo '</tr>';
     }
     echo '</tbody>';
     echo '</table>';
+    echo '<div class="table-summary">';
+    echo '</div>';
 }
 ?>
