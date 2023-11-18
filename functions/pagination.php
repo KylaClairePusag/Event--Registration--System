b30bb144f2dfdde1a26b45e5af271177b5fbba9b
Leo Mikko Uson <lancerthortan@gmail.com>
2023-11-18 13:51:57 +0800

    Fix pagination current page highlighting

--- functions/pagination.php
+++ functions/pagination.php
@@ -16,8 +16,12 @@ function generatePaginationLinks($pdo, $searchTerm, $limit)
         $totalPages = ceil($totalRecords / $limit);
 
         echo "<div class='pagination-container'>";
+
+        // Check if $_GET['page'] is set, otherwise default to 1
+        $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
+
         for ($i = 1; $i <= $totalPages; $i++) {
-            $class = ($i == $_GET['page']) ? 'current-page' : '';
+            $class = ($i == $currentPage) ? 'current-page' : '';
             echo "<a class='$class' href='" . $_SERVER['PHP_SELF'] . "?page=" . $i . "&search=" . urlencode($searchTerm) . "&limit=" . $limit . "'>" . $i . "</a> ";
         }
         echo "</div>";
