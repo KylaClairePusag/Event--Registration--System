a625505969b668bd48251a4e39a5c57f161b2dae
Leo Mikko Uson <lancerthortan@gmail.com>
2023-11-18 13:47:35 +0800

    chore: update rso page

--- admin/dashboard/rso.php
+++ admin/dashboard/rso.php
@@ -18,7 +18,6 @@ $requestUri = $_SERVER['REQUEST_URI'];
 
 // Add new RSO
 if (isset($_POST["add_rso"])) {
-    // Sanitize input
     $rso_name = htmlspecialchars($_POST["rso_name"], ENT_QUOTES, "UTF-8");
     $rso_password = htmlspecialchars($_POST["rso_password"], ENT_QUOTES, "UTF-8");
     $rso_email = htmlspecialchars($_POST["email"], ENT_QUOTES, "UTF-8");
@@ -27,7 +26,7 @@ if (isset($_POST["add_rso"])) {
     // Insert data into the database
     $query = $pdo->prepare("INSERT INTO tb_rso (rso_name, rso_password, rso_email, department_id) VALUES (:rso_name, :rso_password, :rso_email, :department_id)");
     if ($query->execute([':rso_name' => $rso_name, ':rso_password' => $rso_password, ':rso_email' => $rso_email, ':department_id' => $department_id])) {
-        header("Location: " . $requestUri);
+        header("Location: $requestUri");
         exit();
     } else {
         echo "Error adding rso.";
@@ -41,7 +40,7 @@ if (isset($_POST["delete_rso"])) {
     // Delete data from the database
     $query = $pdo->prepare("DELETE FROM tb_rso WHERE rso_id = :rso_id");
     if ($query->execute([':rso_id' => $rso_id])) {
-        header("Location: " . $requestUri);
+        header("Location: $requestUri");
     } else {
         echo "Error deleting rso.";
     }
@@ -58,7 +57,7 @@ if (isset($_POST["edit_rso"])) {
     // Update data in the database
     $query = $pdo->prepare("UPDATE tb_rso SET rso_name = :rso_name, rso_password = :rso_password, rso_email = :rso_email, department_id = :department_id WHERE rso_id = :rso_id");
     if ($query->execute([':rso_name' => $edit_rso_name, ':rso_password' => $edit_rso_password, ':rso_email' => $edit_rso_email, ':department_id' => $edit_department_id, ':rso_id' => $edit_rso_id])) {
-        header("Location: " . $requestUri);
+        header("Location: $requestUri");
         exit();
     } else {
         echo "Error editing rso.";
@@ -95,49 +94,43 @@ try {
 <html>
 
 <head>
-    <title>rso List</title>
-
     <link rel="stylesheet" href="../../styles/rso.css">
-
 </head>
 
 <body>
 
     <?php include '../../components/adminHeader.php'; ?>
+
     <main>
-
         <section class="head">
-
-            <?php
-            include '../../functions/search.php';
-            ?>
+            <div class="searchCont">
+                <?php
+                include '../../functions/search.php';
+                ?>
+                <?php if (!empty($searchTerm)): ?>
+                    <img src='../../images/cross.png' alt='Image' class="icon" onclick="clearSearch()" id='clearBtn' />
+                <?php endif; ?>
+            </div>
             <div class="headbtn">
                 <?php
                 include '../../functions/limit.php';
                 ?>
                 <button type="button" onclick="document.getElementById('addModal').showModal()">Add rso <img
-                        src='../../images/plus.png' alt='Image' /> </button>
-
+                        src='../../images/plus.png' alt='Image' class="icon" /> </button>
             </div>
         </section>
+
         <section class="tableContainer">
-
-            <?php if (!empty($searchTerm)): ?>
-                <button type="button" onclick="clearSearch()">Clear Search</button>
-            <?php endif; ?>
-
-
-
-
             <?php
-            include '../../functions/table.component.php';
+            include '../../table.component.php';
 
-            $head = array('ID', 'Name', 'Email', 'Department', 'Actions'); // Remove 'Password' from the $head array
+            $head = array('ID', 'Name', 'Password', 'Email', 'Department', 'Actions');
             $body = array();
 
             foreach ($rows as $row) {
                 $rso_id = $row["rso_id"];
                 $rso_name = $row["rso_name"];
+                $rso_password = $row["rso_password"];
                 $rso_email = $row["rso_email"];
                 $department_id = $row["department_id"];
                 $department_name = '';
@@ -148,28 +141,31 @@ try {
                     $department_name = $deptRow['department_name'];
                 }
 
-                $actions = '<button type="button" onclick="editRso(' . $rso_id . ', \'' . $rso_name . '\', \'' . $rso_email . '\', \'' . $department_id . '\')">Edit</button> <button type="button" onclick="showDeleteModal(' . $rso_id . ')">Delete</button>';
-                $body[] = array($rso_id, $rso_name, $rso_email, $department_name, $actions); // Remove $rso_password from the $body array
-            
-            }
+                $actions = '<button type="button" onclick="editRso(' . $rso_id . ', \'' . $rso_name . '\', \'' . $rso_password . '\', \'' . $rso_email . '\', \'' . $department_id . '\')">Edit</button> <button type="button" onclick="showDeleteModal(' . $rso_id . ')">Delete</button>';
+                $body[] = array($rso_id, $rso_name, $rso_password, $rso_email, $department_name, $actions);
 
+            }
             createTable($head, $body);
             ?>
 
             <dialog id="addModal" class="modal">
+
                 <div class="modal-content">
                     <button class="close" onclick="resetAddModal(true)">&times;</button>
 
-                    <h2>Add rso</h2>
+                    <h2>CREATE RSO ACCOUNT</h2>
                     <form method="POST" action="">
+                        <div class="error-container">Email Already Taken</div>
+
                         <label for="rso-name">Name:</label>
-                        <input type="text" id="rso-name" name="rso_name">
+                        <input type="text" id="rso-name" name="rso_name" required>
                         <label for="rso-password">Password:</label>
-                        <input type="password" id="rso-password" name="rso_password">
+                        <input type="password" id="rso-password" name="rso_password" required>
                         <label for="rso-email">Email:</label>
-                        <input type="email" id="rso-email" name="email">
+                        <input type="email" id="rso-email" name="email" required>
                         <label for="department">Department:</label>
-                        <select id="department" name="department_id">
+                        <select id="department" name="department_id" required>
+                            <option value="">Select a department</option>
                             <?php
                             $departmentQuery = $pdo->prepare("SELECT department_id, department_name FROM tb_department");
                             $departmentQuery->execute();
@@ -179,21 +175,31 @@ try {
                             }
                             ?>
                         </select>
-                        <div class="error-container">Email Already Taken</div>
-                        <button type="submit" name="add_rso">Add rso</button>
+                        <button type="submit" name="add_rso">Create Rso Account</button>
                     </form>
                 </div>
             </dialog>
             <dialog id="editModal" class="modal">
+
                 <div class="modal-content">
                     <button class="close" onclick="resetAddModal(true)">&times;</button>
-                    <h2>Edit rso</h2>
+
+                    <h2>EDIT RSO ACCOUNT</h2>
+                    <div class="error-container2">Email Already Taken</div>
+
                     <form method="POST" action="">
                         <input type="hidden" id="edit-rso-id" name="edit_rso_id">
                         <label for="edit-rso-name">Name:</label>
                         <input type="text" id="edit-rso-name" name="edit_rso_name">
                         <label for="edit-rso-password">Password:</label>
-                        <input type="password" id="edit-rso-password" name="edit_rso_password">
+
+                        <div class='passcont'>
+                            <input type="password" id="edit-rso-password" name="edit_rso_password">
+                            <img src='../../images/view.png' alt='Show Password' class="icon"
+                                onclick="togglePasswordVisibility('edit-rso-password')" />
+                            <img src='../../images/hide.png' alt='Hide Password' class="icon hide"
+                                onclick="togglePasswordVisibility('edit-rso-password')" />
+                        </div>
                         <label for="edit-email">Email:</label>
                         <input type="email" id="edit-email" name="edit_rso_email">
                         <input type="hidden" id="original-email" value="<?php echo $originalEmail; ?>">
@@ -210,7 +216,6 @@ try {
                             }
                             ?>
                         </select>
-                        <div class="error-container2">Email Already Taken</div>
                         <button type="submit" name="edit_rso">Save</button>
                     </form>
                 </div>
@@ -219,7 +224,7 @@ try {
             <dialog id="deleteModal" class="modal">
                 <div class="modal-content">
                     <button class="close" onclick="closeModal('deleteModal')">&times;</button>
-                    <h2>Delete rso</h2>
+                    <h2>DELETE RSO ACCOUNT</h2>
                     <p>Are you sure you want to delete this rso?</p>
                     <div class="clearfix">
                         <button type="button" class="cancelbtn" onclick="closeModal('deleteModal')">Cancel</button>
@@ -407,7 +412,21 @@ try {
                 errorContainer.style.display = 'none';
             }
         });
+        function togglePasswordVisibility(passwordId) {
+            const passwordInput = document.getElementById(passwordId);
+            const viewIcon = document.querySelector('img[src="../../images/view.png"]');
+            const hideIcon = document.querySelector('img[src="../../images/hide.png"]');
 
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                viewIcon.style.display = "none";
+                hideIcon.style.display = "inline-block";
+            } else {
+                passwordInput.type = "password";
+                viewIcon.style.display = "inline-block";
+                hideIcon.style.display = "none";
+            }
+        }
     </script>
 
 </body>
