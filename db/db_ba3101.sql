a0324d6c855d78c16798a2f04c001d2a9308417f
Kyla Claire Pusag <143685825+KylaClairePusag@users.noreply.github.com>
2023-11-19 20:58:43 +0800

    db_ba3101.sql

--- db/db_ba3101.sql
+++ db/db_ba3101.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 14, 2023 at 04:51 PM
+-- Host: 127.0.0.1
+-- Generation Time: Nov 19, 2023 at 11:53 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -34,6 +34,13 @@ CREATE TABLE `tbempinfo` (
   `department` varchar(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `tbempinfo`
+--
+
+INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
+(1, 'aguila', 'nina', 'cics');
+
 -- --------------------------------------------------------
 
 --
@@ -46,9 +53,17 @@ CREATE TABLE `tbstudinfo` (
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
   `department_id` int(11) DEFAULT NULL,
-  `student_email` varchar(255) NOT NULL,
-  `student_password` varchar(255) NOT NULL
+  `email` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbstudinfo`
+--
+
+INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`, `department_id`, `email`, `password`) VALUES
+(1, 'parker', 'peter', 'bsit', NULL, 'one@gmail.com', '456'),
+(2, 'kent', 'clark', 'bscs', NULL, '0', '');
 
 -- --------------------------------------------------------
 
@@ -60,8 +75,19 @@ CREATE TABLE `tb_admin` (
   `admin_ID` int(11) NOT NULL,
   `admin_name` varchar(255) NOT NULL,
   `admin_password` varchar(20) NOT NULL,
-  `admin_email` varchar(255) NOT NULL
+  `admin_email` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`admin_ID`, `admin_name`, `admin_password`, `admin_email`) VALUES
+(101, 'Francis Balazon', '123', 'me@gmail.com'),
+(102, 'Alimoren Dioneces', 'Dioneces102 ', 'dion@gmail.com'),
+(103, 'Richelle Sulit', 'Sulit103 ', 'sulit@gmail.com'),
+(104, 'Ganiela Catapia', 'Ganiela104 ', 'ganiela@gmail.com'),
+(105, 'Lea Salonga', 'Lea105 ', 'lea@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -88,6 +114,17 @@ CREATE TABLE `tb_department` (
   `department_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `tb_department`
+--
+
+INSERT INTO `tb_department` (`department_id`, `department_name`) VALUES
+(2201, 'CICS'),
+(2202, 'CABE'),
+(2203, 'YPMAP'),
+(2204, 'CICS'),
+(2205, 'CABE');
+
 -- --------------------------------------------------------
 
 --
@@ -99,9 +136,21 @@ CREATE TABLE `tb_event` (
   `event_title` varchar(255) NOT NULL,
   `event_detail` text NOT NULL,
   `event_date` date NOT NULL,
-  `department_id` int(11) DEFAULT NULL,
-  `status` varchar(50) DEFAULT NULL
+  `department_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_event`
+--
+
+INSERT INTO `tb_event` (`event_id`, `event_title`, `event_detail`, `event_date`, `department_id`) VALUES
+(3101, 'SportFest', 'Gather everyone', '2023-10-28', NULL),
+(3102, 'Night Fest', 'Gather Everyone', '2023-10-29', NULL),
+(3103, 'Valentines Day', 'Gather Everyone', '2024-02-14', NULL),
+(3104, 'CICS Day', 'Gather Everyone', '2023-10-31', NULL),
+(3105, 'Christmas Party', 'Gather Everyone', '2023-12-22', NULL),
+(3106, 'Hackathon', 'Venue: CICS Building Room 405', '2023-10-02', 2201),
+(3108, 'Hackathon 2', 'xdxdxd', '2023-11-12', 2202);
 
 -- --------------------------------------------------------
 
@@ -114,7 +163,29 @@ CREATE TABLE `tb_faculty` (
   `faculty_name` varchar(255) NOT NULL,
   `faculty_password` varchar(20) NOT NULL,
   `department_id` int(11) DEFAULT NULL,
-  `faculty_email` varchar(255) NOT NULL
+  `email` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_faculty`
+--
+
+INSERT INTO `tb_faculty` (`faculty_id`, `faculty_name`, `faculty_password`, `department_id`, `email`) VALUES
+(1, 'CICS', '111', NULL, 'iu@gmail.com'),
+(2, 'CICS', '02BSUCICS', NULL, '0'),
+(3, 'CICS', '03BSUCICS', NULL, '0'),
+(4, 'CICS', '04BSUCICS', NULL, '0'),
+(5, 'CICS', '05BSUCICS', NULL, '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_register`
+--
+
+CREATE TABLE `tb_register` (
+  `event_id` int(11) NOT NULL,
+  `email` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -128,8 +199,19 @@ CREATE TABLE `tb_rso` (
   `rso_name` varchar(255) NOT NULL,
   `rso_password` varchar(20) NOT NULL,
   `department_id` int(11) DEFAULT NULL,
-  `rso_email` varchar(255) NOT NULL
+  `email` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_rso`
+--
+
+INSERT INTO `tb_rso` (`rso_id`, `rso_name`, `rso_password`, `department_id`, `email`) VALUES
+(1101, 'CICS', '000', NULL, 'me@gmail.com'),
+(1102, 'CABE', '1101CABE', NULL, '0'),
+(1103, 'YPMAP', '1101YPMAP', NULL, '0'),
+(1104, 'JPCS', '1101JPCS', NULL, '0'),
+(1105, 'CIT', '1105CIT', NULL, '0');
 
 --
 -- Indexes for dumped tables
@@ -152,13 +234,16 @@ ALTER TABLE `tbstudinfo`
 -- Indexes for table `tb_admin`
 --
 ALTER TABLE `tb_admin`
-  ADD PRIMARY KEY (`admin_ID`);
+  ADD PRIMARY KEY (`admin_ID`),
+  ADD UNIQUE KEY `admin_email` (`admin_email`);
 
 --
 -- Indexes for table `tb_attendees`
 --
 ALTER TABLE `tb_attendees`
   ADD PRIMARY KEY (`attendee_id`),
+  ADD UNIQUE KEY `rso_id_2` (`rso_id`),
+  ADD UNIQUE KEY `rso_id_3` (`rso_id`),
   ADD KEY `event_id` (`event_id`),
   ADD KEY `student_id` (`student_id`),
   ADD KEY `faculty_id` (`faculty_id`),
@@ -205,19 +290,19 @@ ALTER TABLE `tbempinfo`
 -- AUTO_INCREMENT for table `tbstudinfo`
 --
 ALTER TABLE `tbstudinfo`
-  MODIFY `studid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `studid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tb_admin`
 --
 ALTER TABLE `tb_admin`
-  MODIFY `admin_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=133;
+  MODIFY `admin_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
 
 --
 -- AUTO_INCREMENT for table `tb_attendees`
 --
 ALTER TABLE `tb_attendees`
-  MODIFY `attendee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `attendee_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_department`
@@ -229,19 +314,19 @@ ALTER TABLE `tb_department`
 -- AUTO_INCREMENT for table `tb_event`
 --
 ALTER TABLE `tb_event`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3107;
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3109;
 
 --
 -- AUTO_INCREMENT for table `tb_faculty`
 --
 ALTER TABLE `tb_faculty`
-  MODIFY `faculty_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `faculty_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_rso`
 --
 ALTER TABLE `tb_rso`
-  MODIFY `rso_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1168;
+  MODIFY `rso_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1106;
 
 --
 -- Constraints for dumped tables
