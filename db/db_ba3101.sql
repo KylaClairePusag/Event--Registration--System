e04cff9a86808ab65495fda7451cfe8a0a5ebe31
Marvin Kristian M. Cruz <143852958+yeonaaa0411@users.noreply.github.com>
2023-11-20 18:32:31 +0800

    Inserting Admin and Student Data

--- db/db_ba3101.sql
+++ db/db_ba3101.sql
@@ -1,15 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 20, 2023 at 07:51 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Nov 20, 2023 at 11:29 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.0.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -33,9 +35,48 @@ CREATE TABLE `tbstudinfo` (
   `course` varchar(20) NOT NULL,
   `department_id` int(11) DEFAULT NULL,
   `student_email` varchar(255) NOT NULL,
-  `student_profile` longblob DEFAULT NULL,
+  `student_profile` longblob,
   `student_password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbstudinfo`
+--
+
+INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`, `department_id`, `student_email`, `student_profile`, `student_password`) VALUES
+(-39889, 'GALVERO', 'MATTHEW DANIELL', 'BSIT', 3101, '21-39910@g.batstate-u.edu.ph', NULL, '123'),
+(-39807, 'MANALO', 'ZEUS ANDREI', 'BSIT', 3101, '21-39828@g.batstate-u.edu.ph', NULL, '123'),
+(-39797, 'FRUELDA', 'MARK JOSEPH', 'BSIT', 3101, '21-39818@g.batstate-u.edu.ph', NULL, '123'),
+(-39733, 'DIMAANO', 'JOSHUA', 'BSIT', 3101, '21-39754@g.batstate-u.edu.ph', NULL, '123'),
+(-39393, 'RUBION', 'IVAN GABRIEL', 'BSIT', 3101, '21-39414@g.batstate-u.edu.ph', NULL, '123'),
+(-39320, 'OLAN', 'JAKE DARREN', 'BSIT', 3101, '21-39341@g.batstate-u.edu.ph', NULL, '123'),
+(-39084, 'HERNANDEZ', 'AARON CHRISTIAN', 'BSIT', 3101, '21-39105@g.batstate-u.edu.ph', NULL, '123'),
+(-38481, 'CARILLO', 'JAMES KEOCH', 'BSIT', 3101, '21-38502@g.batstate-u.edu.ph', NULL, '123'),
+(-38270, 'PURIO', 'JOHN ANDREI', 'BSIT', 3101, '21-38291@g.batstate-u.edu.ph', NULL, '123'),
+(-37455, 'TELESFORO', 'LEANNE FRANK', 'BSIT', 3101, '21-37476@g.batstate-u.edu.ph', NULL, '123'),
+(-37431, 'ACDA', 'CHRISTIAN RAFAEL', 'BSIT', 3101, '21-37452@g.batstate-u.edu.ph', NULL, '123'),
+(-36658, 'CASTILLO', 'DIANNE KRISTEL', 'BSIT', 3101, '21-36679@g.batstate-u.edu.ph', NULL, '123'),
+(-35567, 'CUYA', 'LESTER RHOY', 'BSIT', 3101, '21-35588@g.batstate-u.edu.ph', NULL, '123'),
+(-35498, 'DE CHAVEZ', 'JHUNCEN', 'BSIT', 3101, '21-35519@g.batstate-u.edu.ph', NULL, '123'),
+(-34973, 'CEREZO', 'EZEKIEL EISEN', 'BSIT', 3101, '21-34994@g.batstate-u.edu.ph', NULL, '123'),
+(-34154, 'FABELLON', 'JAN PATRICK', 'BSIT', 3101, '21-34175@g.batstate-u.edu.ph', NULL, '123'),
+(-33759, 'BACONG', 'JOHN OLIVER', 'BSIT', 3101, '21-33780@g.batstate-u.edu.ph', NULL, '123'),
+(-33457, 'PEREZ', 'JAZZMIN', 'BSIT', 3101, '21-33478@g.batstate-u.edu.ph', NULL, '123'),
+(-33036, 'ALDAU', 'KEON CHESTER', 'BSIT', 3101, '21-33057@g.batstate-u.edu.ph', NULL, '123'),
+(-32963, 'CANIETE', 'CRISTEL MAE ', 'BSIT', 3101, '21-32984@g.batstate-u.edu.ph', NULL, '123'),
+(-32450, ' MACALLA', 'DOROTHY', 'BSIT', 3101, '21-32471@g.batstate-u.edu.ph', NULL, '123'),
+(-31341, 'POBLETE', 'JAMES REMUEL', 'BSIT', 3101, '21-31362@g.batstate-u.edu.ph', NULL, '123'),
+(-31033, ' TADA', 'SETH ALDOUS', 'BSIT', 3101, '21-31054@g.batstate-u.edu.ph', NULL, '123'),
+(-30843, 'MARASIGAN', 'HARVEY NICOLE', 'BSIT', 3101, '21-30864@g.batstate-u.edu.ph', NULL, '123'),
+(-30823, 'RAZ', 'DANNIEL DAVID', 'BSIT', 3101, '21-30844@g.batstate-u.edu.ph', NULL, '123'),
+(-30776, 'NOVICIO', 'NICKO LOUISE', 'BSIT', 3101, '21-30797@g.batstate-u.edu.ph', NULL, '123'),
+(-30734, 'SOLIS II', 'ROBERTO ANTONIO', 'BSIT', 3101, '21-30755@g.batstate-u.edu.ph', NULL, '123'),
+(-30542, 'MACARAIG', 'NIEL KEVIN', 'BSIT', 3101, '21-30563@g.batstate-u.edu.ph', NULL, '123'),
+(-30360, 'FABREGAS', 'IRELLE KERVIN', 'BSIT', 3101, '21-30381@g.batstate-u.edu.ph', NULL, '123'),
+(-30052, 'TRAYFALGAR', 'BELLE COLLEEN', 'BSIT', 3101, '21-30073@g.batstate-u.edu.ph', NULL, '123'),
+(-29987, 'MAGSINO', 'JOHN ADERIEL', 'BSIT', 3101, '21-30008@g.batstate-u.edu.ph', NULL, '123');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tb_admin`
@@ -46,8 +87,17 @@ CREATE TABLE `tb_admin` (
   `admin_name` varchar(255) NOT NULL,
   `admin_password` varchar(20) NOT NULL,
   `admin_email` varchar(255) NOT NULL,
-  `admin_profile` longblob DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `admin_profile` longblob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`admin_ID`, `admin_name`, `admin_password`, `admin_email`, `admin_profile`) VALUES
+(-39284, 'Marvin Kristian M. Cruz', 'marvincute', '21-39305@g.batstate-u.edu.ph', NULL);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tb_attendees`
@@ -59,7 +109,7 @@ CREATE TABLE `tb_attendees` (
   `student_id` int(11) DEFAULT NULL,
   `faculty_id` int(11) DEFAULT NULL,
   `rso_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -70,7 +120,9 @@ CREATE TABLE `tb_attendees` (
 CREATE TABLE `tb_department` (
   `department_id` int(11) NOT NULL,
   `department_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tb_event`
@@ -81,10 +133,10 @@ CREATE TABLE `tb_event` (
   `event_title` varchar(255) NOT NULL,
   `event_detail` text NOT NULL,
   `event_date` date NOT NULL,
-  `header_image` longblob DEFAULT NULL,
+  `header_image` longblob,
   `department_id` int(11) DEFAULT NULL,
   `status` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -96,7 +148,7 @@ CREATE TABLE `tb_event_images` (
   `image_id` int(11) NOT NULL,
   `event_id` int(11) DEFAULT NULL,
   `image_filename` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -110,8 +162,8 @@ CREATE TABLE `tb_faculty` (
   `faculty_password` varchar(20) NOT NULL,
   `department_id` int(11) DEFAULT NULL,
   `faculty_email` varchar(255) NOT NULL,
-  `faculty_profile` longblob DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `faculty_profile` longblob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -125,10 +177,8 @@ CREATE TABLE `tb_rso` (
   `rso_password` varchar(20) NOT NULL,
   `department_id` int(11) DEFAULT NULL,
   `rso_email` varchar(255) NOT NULL,
-  `rso_profile` longblob DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+  `rso_profile` longblob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -140,165 +190,6 @@ CREATE TABLE `tb_rso` (
 ALTER TABLE `tbstudinfo`
   ADD PRIMARY KEY (`studid`),
   ADD KEY `fk_department` (`department_id`);
-
---
--- Indexes for table `tb_admin`
---
-ALTER TABLE `tb_admin`
-  ADD PRIMARY KEY (`admin_ID`),
-  ADD UNIQUE KEY `unique_admin_email` (`admin_email`);
-
---
--- Indexes for table `tb_attendees`
---
-ALTER TABLE `tb_attendees`
-  ADD PRIMARY KEY (`attendee_id`),
-  ADD KEY `event_id` (`event_id`),
-  ADD KEY `student_id` (`student_id`),
-  ADD KEY `faculty_id` (`faculty_id`),
-  ADD KEY `rso_id` (`rso_id`);
-
---
--- Indexes for table `tb_department`
---
-ALTER TABLE `tb_department`
-  ADD PRIMARY KEY (`department_id`);
-
---
--- Indexes for table `tb_event`
---
-ALTER TABLE `tb_event`
-  ADD PRIMARY KEY (`event_id`),
-  ADD KEY `department_id` (`department_id`);
-
---
--- Indexes for table `tb_event_images`
---
-ALTER TABLE `tb_event_images`
-  ADD PRIMARY KEY (`image_id`),
-  ADD KEY `event_id` (`event_id`);
-
---
--- Indexes for table `tb_faculty`
---
-ALTER TABLE `tb_faculty`
-  ADD PRIMARY KEY (`faculty_id`),
-  ADD KEY `department_id` (`department_id`);
-
---
--- Indexes for table `tb_rso`
---
-ALTER TABLE `tb_rso`
-  ADD PRIMARY KEY (`rso_id`),
-  ADD UNIQUE KEY `unique_rso_email` (`rso_email`),
-  ADD KEY `department_id` (`department_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tbstudinfo`
---
-ALTER TABLE `tbstudinfo`
-  MODIFY `studid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `tb_admin`
---
-ALTER TABLE `tb_admin`
-  MODIFY `admin_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=208;
-
---
--- AUTO_INCREMENT for table `tb_attendees`
---
-ALTER TABLE `tb_attendees`
-  MODIFY `attendee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `tb_department`
---
-ALTER TABLE `tb_department`
-  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2206;
-
---
--- AUTO_INCREMENT for table `tb_event`
---
-ALTER TABLE `tb_event`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3138;
-
---
--- AUTO_INCREMENT for table `tb_event_images`
---
-ALTER TABLE `tb_event_images`
-  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-
---
--- AUTO_INCREMENT for table `tb_faculty`
---
-ALTER TABLE `tb_faculty`
-  MODIFY `faculty_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `tb_rso`
---
-ALTER TABLE `tb_rso`
-  MODIFY `rso_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1435;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `tbstudinfo`
---
-ALTER TABLE `tbstudinfo`
-  ADD CONSTRAINT `fk_department` FOREIGN KEY (`department_id`) REFERENCES `tb_department` (`department_id`);
-
---
--- Constraints for table `tb_attendees`
---
-ALTER TABLE `tb_attendees`
-  ADD CONSTRAINT `tb_attendees_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `tb_event` (`event_id`),
-  ADD CONSTRAINT `tb_attendees_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `tbstudinfo` (`studid`) ON DELETE CASCADE,
-  ADD CONSTRAINT `tb_attendees_ibfk_3` FOREIGN KEY (`faculty_id`) REFERENCES `tb_faculty` (`faculty_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `tb_attendees_ibfk_4` FOREIGN KEY (`rso_id`) REFERENCES `tb_rso` (`rso_id`) ON DELETE CASCADE;
-
---
--- Constraints for table `tb_event`
---
-ALTER TABLE `tb_event`
-  ADD CONSTRAINT `tb_event_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `tb_department` (`department_id`);
-
---
--- Constraints for table `tb_event_images`
---
-ALTER TABLE `tb_event_images`
-  ADD CONSTRAINT `tb_event_images_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `tb_event` (`event_id`) ON DELETE CASCADE;
-
---
--- Constraints for table `tb_faculty`
---
-ALTER TABLE `tb_faculty`
-  ADD CONSTRAINT `tb_faculty_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `tb_department` (`department_id`);
-
---
--- Constraints for table `tb_rso`
---
-ALTER TABLE `tb_rso`
-  ADD CONSTRAINT `tb_rso_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `tb_department` (`department_id`);
-
-DELIMITER $$
---
--- Events
---
-CREATE DEFINER=`root`@`localhost` EVENT `update_event_status` ON SCHEDULE EVERY 1 SECOND STARTS '2023-11-20 12:35:58' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
-    UPDATE `tb_event` SET `status` = 'upcoming' WHERE `event_date` > CURDATE();
-    UPDATE `tb_event` SET `status` = 'ongoing' WHERE `event_date` = CURDATE();
-    UPDATE `tb_event` SET `status` = 'ended' WHERE `event_date` < CURDATE();
-END$$
-
-DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
