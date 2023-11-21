70dfb9fc47d2ccba5d7178fd2016e08340500615
VerlonJohnMorales <21-37516@g.batstate-u.edu.ph>
2023-11-21 14:56:32 +0800

    added modal confirmation

--- student/home/index.php
+++ student/home/index.php
@@ -1,13 +1,49 @@
 <?php
-// Database connection setup
 include '../../config/config.php';
-session_start();
 
 $conn = new mysqli('localhost', 'root', '', 'db_ba3101');
 if ($conn->connect_error) {
     die('Connection Failed: ' . $conn->connect_error);
 }
 
+// Assuming $pdo is the PDO instance, but it's not defined in your code
+// Make sure to create a PDO instance before using it
+$pdo = new PDO('mysql:host=localhost;dbname=db_ba3101', 'root', '');
+
+$checkQuery = "SELECT * FROM tb_attendees WHERE event_id = :event_id AND student_id = :student_id";
+$checkStmt = $pdo->prepare($checkQuery);
+$checkStmt->bindParam(':event_id', $event_id, PDO::PARAM_INT);
+$checkStmt->bindParam(':student_id', $_SESSION['student_id'], PDO::PARAM_INT);
+$checkStmt->execute();
+$attendeeExists = $checkStmt->rowCount() > 0;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['attend'])) {
+    if (!$attendeeExists) {
+        $query = "INSERT INTO tb_attendees (event_id, student_id) VALUES (:event_id, :student_id)";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':event_id', $_POST['event_id'], PDO::PARAM_INT);
+        $stmt->bindParam(':student_id', $_SESSION['student_id'], PDO::PARAM_INT);
+        $stmt->execute();
+    }
+
+    header("Location: index.php");
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['cancel'])) {
+    $deleteQuery = "DELETE FROM tb_attendees WHERE event_id = :event_id AND student_id = :student_id";
+    $deleteStmt = $pdo->prepare($deleteQuery);
+    $deleteStmt->bindParam(':event_id', $_POST['event_id'], PDO::PARAM_INT);
+    $deleteStmt->bindParam(':student_id', $_SESSION['student_id'], PDO::PARAM_INT);
+    $deleteStmt->execute();
+
+    $_SESSION['delete_action_completed'] = true;
+
+    header("Location: index.php");
+    exit;
+}
+
+// Fetch events data
 try {
     // Retrieve department_id from the session
     $departmentId = $_SESSION['department_id'];
@@ -30,13 +66,24 @@ try {
     die();
 }
 ?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Page</title>
     <link rel="stylesheet" href="../../styles/student.css">
     <link rel="stylesheet" href="../../styles/rso.css">
+
+    <style>
+        /* Add some styles for the dialog */
+        dialog {
+            padding: 20px;
+            background-color: #fff;
+            z-index: 1000;
+        }
+    </style>
 </head>
 
 <body>
@@ -44,42 +91,43 @@ try {
 
     <main>
         <div class="box-container">
-            <?php
-            foreach ($rows as $row) {
-                $event_id = $row["event_id"];
-                $event_title = $row["event_title"];
-                $event_detail = $row["event_detail"];
-                $event_date = date('F j, Y', strtotime($row["event_date"]));
-                $status = $row["status"];
-                $header_image = $row["header_image"];
-                $department_name = $row["department_name"];
-
-                $attendees_query = $conn->prepare("SELECT COUNT(*) as attendee_count FROM tb_attendees WHERE event_id = ?");
-                $attendees_query->bind_param('i', $event_id);
-                $attendees_query->execute();
-                $attendees_result = $attendees_query->get_result();
-                $attendees_row = $attendees_result->fetch_assoc();
-                $attendee_count = $attendees_row['attendee_count'];
-
-                // Define a CSS class based on the status
-                $statusClass = '';
-                switch ($status) {
-                    case 'upcoming':
-                        $statusClass = 'upcoming-event';
-                        break;
-                    case 'ongoing':
-                        $statusClass = 'ongoing-event';
-                        break;
-                    case 'ended':
-                        $statusClass = 'ended-event';
-                        break;
-                    default:
-                        $statusClass = 'default-event';
-                        break;
-                }
-                ?>
-
+            <?php foreach ($rows as $row) { ?>
                 <div class="event-box">
+                    <?php
+                    // Extract event details
+                    $event_id = $row["event_id"];
+                    $event_title = $row["event_title"];
+                    $event_detail = $row["event_detail"];
+                    $event_date = date('F j, Y', strtotime($row["event_date"]));
+                    $status = $row["status"];
+                    $header_image = $row["header_image"];
+                    $department_name = $row["department_name"];
+
+                    // Count attendees for the event
+                    $attendees_query = $conn->prepare("SELECT COUNT(*) as attendee_count FROM tb_attendees WHERE event_id = ?");
+                    $attendees_query->bind_param('i', $event_id);
+                    $attendees_query->execute();
+                    $attendees_row = $attendees_query->get_result()->fetch_assoc();
+                    $attendee_count = $attendees_row['attendee_count'];
+
+                    // Define a CSS class based on the status
+                    $statusClass = '';
+                    switch ($status) {
+                        case 'upcoming':
+                            $statusClass = 'upcoming-event';
+                            break;
+                        case 'ongoing':
+                            $statusClass = 'ongoing-event';
+                            break;
+                        case 'ended':
+                            $statusClass = 'ended-event';
+                            break;
+                        default:
+                            $statusClass = 'default-event';
+                            break;
+                    }
+                    ?>
+
                     <div class="header">
                         <img src="../../<?php echo $header_image; ?>" alt="Event Image">
                     </div>
@@ -107,20 +155,106 @@ try {
                         </p>
                     </div>
                     <div class="events_button">
-                        <button id="viewBtn"
-                            onclick="window.location.href='event.php?event_id=<?php echo $event_id; ?>'">View
-                            Event</button>
-                        <?php
-                        include '../../components/attendButton.php';
-                        generateAttenButton($event_id, $pdo);
-                        ?>
+                        <form action="" method="POST">
+                            <button type="button" id="viewBtn"
+                                onclick="window.location.href='event.php?event_id=<?php echo $event_id; ?>'">View
+                                Event</button>
+                            <input type="hidden" name="event_id" value="<?php echo $event_id; ?>">
+
+                            <?php
+                            // Move the attendee check inside the loop
+                            $attendeeExists = false; // Default to false for each event
+                            $checkStmt->execute();
+                            $attendeeExists = $checkStmt->rowCount() > 0;
+
+                            if ($attendeeExists) {
+                                echo '<button type="button" onclick="openCancelModal(' . $event_id . ')">Cancel</button>';
+                            } else {
+                                echo '<button type="button" onclick="openAttendModal(' . $event_id . ')">Interested</button>';
+                            }
+                            ?>
+                        </form>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+
+        <!-- The Attend Dialog -->
+        <dialog id="attendDialog" class="modal">
+            <div class="modal-content">
+
+                <button class="close" onclick="closeCancelDialog()">&times;</button>
+                <h2>Are you sure you want to attend this event?</h2>
+                <form action="" method="POST">
+                    <input type="hidden" name="event_id" id="attendDialogEventId" value="">
+                    <div class="clearfix">
+                        <button type="button" onclick="closeAttendDialog()" class="cancelbtn">No</button>
+
+                        <button type="submit" name="attend">Yes</button>
+
+
                     </div>
 
-
-                <?php } ?>
-
+                </form>
             </div>
+        </dialog>
+
+        <!-- The Cancel Dialog -->
+        <dialog id="cancelDialog" class="modal">
+            <div class="modal-content">
+                <button class="close" onclick="closeCancelDialog()">&times;</button>
+                <h2>Are you sure you want to cancel your attendance for this event?</h2>
+                <form action="" method="POST">
+                    <input type="hidden" name="event_id" id="cancelDialogEventId" value="">
+                    <div class="clearfix">
+                        <button type="button" onclick="closeCancelDialog()" class="cancelbtn">No</button>
+
+                        <button type="submit" name="cancel">Yes</button>
+
+
+                    </div>
+                </form>
+            </div>
+
+        </dialog>
+
+        <script>
+            // JavaScript functions to handle dialogs
+            function openAttendModal(eventId) {
+                document.getElementById('attendDialogEventId').value = eventId;
+                document.getElementById('attendDialog').showModal();
+            }
+
+            function closeAttendDialog() {
+                document.getElementById('attendDialog').close();
+            }
+
+            function openCancelModal(eventId) {
+                document.getElementById('cancelDialogEventId').value = eventId;
+                document.getElementById('cancelDialog').showModal();
+            }
+
+            function closeCancelDialog() {
+                document.getElementById('cancelDialog').close();
+            }
+
+            // Close dialog if user clicks outside
+            document.addEventListener('click', function (event) {
+                var modal = document.querySelector('.modal');
+                if (event.target === modal) {
+                    modal.close();
+                }
+            });
+            document.addEventListener('click', function (event) {
+                var cancelModal = document.getElementById('cancelDialog');
+                if (event.target === cancelModal) {
+                    cancelModal.close();
+                }
+            });
+        </script>
+
     </main>
+
 </body>
 
 </html>
