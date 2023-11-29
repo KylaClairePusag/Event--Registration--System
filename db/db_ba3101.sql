c7e3eaafcdb29b51e4be8b9f969a4a2e614d1716
Leo Mikko Uson <usonleomikko@gmail.com>
2023-11-29 22:56:51 +0800

    update schema

--- db/db_ba3101.sql
+++ db/db_ba3101.sql
@@ -19,48 +19,48 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: db_ba3101
+-- Database: `db_ba3101`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table tbempinfo
+-- Table structure for table `tbempinfo`
 --
 
-DROP TABLE IF EXISTS tbempinfo;
-CREATE TABLE IF NOT EXISTS tbempinfo (
-  empid int(11) NOT NULL AUTO_INCREMENT,
-  lastname varchar(25) NOT NULL,
-  firstname varchar(25) NOT NULL,
-  department varchar(20) NOT NULL,
-  PRIMARY KEY (empid)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `tbempinfo`;
+CREATE TABLE IF NOT EXISTS `tbempinfo` (
+  `empid` int(11) NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `department` varchar(20) NOT NULL,
+  PRIMARY KEY (`empid`)
+) ;
 
 --
--- Dumping data for table tbempinfo
+-- Dumping data for table `tbempinfo`
 --
 
-INSERT INTO tbempinfo (empid, lastname, firstname, department) VALUES
+INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
 (1, 'aguila', 'nina', 'cics');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table tb_department
+-- Table structure for table `tb_department`
 --
 
-CREATE TABLE tb_department (
-  department_id int(11) NOT NULL AUTO_INCREMENT,
-  department_name varchar(255) NOT NULL,
-  PRIMARY KEY (department_id)
+CREATE TABLE `tb_department` (
+  `department_id` int(11) NOT NULL AUTO_INCREMENT,
+  `department_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`department_id`)
 );
 
 --
--- Dumping data for table tb_department
+-- Dumping data for table `tb_department`
 --
 
-INSERT INTO tb_department (department_id, department_name) VALUES
+INSERT INTO `tb_department` (`department_id`, `department_name`) VALUES
 (1, 'CICS'),
 (2, 'CABE'),
 (3, 'CAS');
@@ -68,161 +68,161 @@ INSERT INTO tb_department (department_id, department_name) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table tb_roles
+-- Table structure for table `tb_roles`
 --
 
-CREATE TABLE tb_roles (
-  role_id int(11) NOT NULL AUTO_INCREMENT,
-  role_name varchar(50) NOT NULL,
-  PRIMARY KEY (role_id),
-  UNIQUE KEY unique_role_name (role_name)
+CREATE TABLE `tb_roles` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`role_id`),
+  UNIQUE KEY `unique_role_name` (`role_name`)
 );
 
 
 --
--- Dumping data for table tb_roles
+-- Dumping data for table `tb_roles`
 --
 
-INSERT INTO tb_roles (role_id, role_name) VALUES
+INSERT INTO `tb_roles` (`role_id`, `role_name`) VALUES
 (1, 'Admin'),
 (2, 'Teacher');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table tbempaccount
+-- Table structure for table `tbempaccount`
 --
 
-CREATE TABLE tbempaccount (
-  empaccountId int(11) NOT NULL AUTO_INCREMENT,
-  empid int(11) NOT NULL,
-  department_id int(11) DEFAULT NULL,
-  emp_email varchar(255) NOT NULL,
-  emp_profile longblob DEFAULT NULL,
-  emp_password varchar(255) NOT NULL,
-  role_id int(11) DEFAULT NULL,
-  PRIMARY KEY (empaccountId),
-  KEY fk_empinfo_changes (empid),
-  KEY fk_department_emp (department_id),
-  KEY fk_emp_roles (role_id),
-  CONSTRAINT fk_department_emp FOREIGN KEY (department_id) REFERENCES tb_department (department_id) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT fk_emp_roles FOREIGN KEY (role_id) REFERENCES tb_roles (role_id) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT fk_empinfo_changes FOREIGN KEY (empid) REFERENCES tbempinfo (empid) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `tbempaccount` (
+  `empaccountId` int(11) NOT NULL AUTO_INCREMENT,
+  `empid` int(11) NOT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `emp_email` varchar(255) NOT NULL,
+  `emp_profile` longblob DEFAULT NULL,
+  `emp_password` varchar(255) NOT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`empaccountId`),
+  KEY `fk_empinfo_changes` (`empid`),
+  KEY `fk_department_emp` (`department_id`),
+  KEY `fk_emp_roles` (`role_id`),
+  CONSTRAINT `fk_department_emp` FOREIGN KEY (`department_id`) REFERENCES `tb_department` (`department_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_emp_roles` FOREIGN KEY (`role_id`) REFERENCES `tb_roles` (`role_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_empinfo_changes` FOREIGN KEY (`empid`) REFERENCES `tbempinfo` (`empid`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table tbstudinfo
+-- Table structure for table `tbstudinfo`
 --
 
-DROP TABLE IF EXISTS tbstudinfo;
-CREATE TABLE IF NOT EXISTS tbstudinfo (
-  studid int(11) NOT NULL AUTO_INCREMENT,
-  lastname varchar(25) NOT NULL,
-  firstname varchar(25) NOT NULL,
-  course varchar(20) NOT NULL,
-  PRIMARY KEY (studid)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `tbstudinfo`;
+CREATE TABLE IF NOT EXISTS `tbstudinfo` (
+  `studid` int(11) NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `course` varchar(20) NOT NULL,
+  PRIMARY KEY (`studid`)
+) ;
 
 --
--- Dumping data for table tbstudinfo
+-- Dumping data for table `tbstudinfo`
 --
 
-INSERT INTO tbstudinfo (studid, lastname, firstname, course) VALUES
+INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
 (1, 'parker', 'peter', 'bsit'),
 (2, 'kent', 'clark', 'bscs');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table tbstudentaccount
+-- Table structure for table `tbstudentaccount`
 --
 
-CREATE TABLE tbstudentaccount (
-  studaccountid int(11) NOT NULL AUTO_INCREMENT,
-  studid int(11) NOT NULL,
-  department_id int(11) DEFAULT NULL,
-  student_email varchar(255) NOT NULL,
-  student_profile longblob DEFAULT NULL,
-  student_password varchar(255) NOT NULL,
-  PRIMARY KEY (studaccountid),
-  KEY fk_department_student (department_id),
-  CONSTRAINT fk_studinfo_changes FOREIGN KEY (studid) REFERENCES tbstudinfo (studid) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_department_student FOREIGN KEY (department_id) REFERENCES tb_department (department_id) ON DELETE SET NULL ON UPDATE CASCADE
+CREATE TABLE `tbstudentaccount` (
+  `studaccountid` int(11) NOT NULL AUTO_INCREMENT,
+  `studid` int(11) NOT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `student_email` varchar(255) NOT NULL,
+  `student_profile` longblob DEFAULT NULL,
+  `student_password` varchar(255) NOT NULL,
+  PRIMARY KEY (`studaccountid`),
+  KEY `fk_department_student` (`department_id`),
+  CONSTRAINT `fk_studinfo_changes` FOREIGN KEY (`studid`) REFERENCES `tbstudinfo` (`studid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_department_student` FOREIGN KEY (`department_id`) REFERENCES `tb_department` (`department_id`) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table tb_rso
+-- Table structure for table `tb_rso`
 --
 
-CREATE TABLE tb_rso (
-  rso_id int(11) NOT NULL AUTO_INCREMENT,
-  rso_name varchar(255) NOT NULL,
-  rso_password varchar(20) NOT NULL,
-  department_id int(11) DEFAULT NULL,
-  rso_email varchar(255) NOT NULL,
-  rso_profile longblob DEFAULT NULL,
-  PRIMARY KEY (rso_id),
-  UNIQUE KEY unique_rso_email (rso_email),
-  KEY department_id (department_id),
-  CONSTRAINT fk_rso_department FOREIGN KEY (department_id) REFERENCES tb_department (department_id) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `tb_rso` (
+  `rso_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rso_name` varchar(255) NOT NULL,
+  `rso_password` varchar(20) NOT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `rso_email` varchar(255) NOT NULL,
+  `rso_profile` longblob DEFAULT NULL,
+  PRIMARY KEY (`rso_id`),
+  UNIQUE KEY `unique_rso_email` (`rso_email`),
+  KEY `department_id` (`department_id`),
+  CONSTRAINT `fk_rso_department` FOREIGN KEY (`department_id`) REFERENCES `tb_department` (`department_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table tb_event
+-- Table structure for table `tb_event`
 --
 
-CREATE TABLE tb_event (
-  event_id int(11) NOT NULL AUTO_INCREMENT,
-  event_title varchar(255) NOT NULL,
-  event_detail text NOT NULL,
-  event_date date NOT NULL,
-  header_image longblob DEFAULT NULL,
-  department_id int(11) DEFAULT NULL,
-  status varchar(50) DEFAULT NULL,
-  PRIMARY KEY (event_id),
-  KEY department_id (department_id),
-  CONSTRAINT fk_event_department FOREIGN KEY (department_id) REFERENCES tb_department (department_id) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `tb_event` (
+  `event_id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_title` varchar(255) NOT NULL,
+  `event_detail` text NOT NULL,
+  `event_date` date NOT NULL,
+  `header_image` longblob DEFAULT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`event_id`),
+  KEY `department_id` (`department_id`),
+  CONSTRAINT `fk_event_department` FOREIGN KEY (`department_id`) REFERENCES `tb_department` (`department_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table tb_event_images
+-- Table structure for table `tb_event_images`
 --
 
-CREATE TABLE tb_event_images (
-  image_id int(11) NOT NULL AUTO_INCREMENT,
-  event_id int(11) DEFAULT NULL,
-  image_filename varchar(255) DEFAULT NULL,
-  PRIMARY KEY (image_id),
-  KEY event_id (event_id),
-  CONSTRAINT fk_image_event FOREIGN KEY (event_id) REFERENCES tb_event (event_id) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `tb_event_images` (
+  `image_id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_id` int(11) DEFAULT NULL,
+  `image_filename` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`image_id`),
+  KEY `event_id` (`event_id`),
+  CONSTRAINT `fk_image_event` FOREIGN KEY (`event_id`) REFERENCES `tb_event` (`event_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table tb_attendees
+-- Table structure for table `tb_attendees`
 --
 
-CREATE TABLE tb_attendees (
-  attendee_id int(11) NOT NULL AUTO_INCREMENT,
-  event_id int(11) NOT NULL,
-  student_id int(11) DEFAULT NULL,
-  empid int(11) DEFAULT NULL,
-  PRIMARY KEY (attendee_id),
-  KEY event_id (event_id),
-  KEY student_id (student_id),
-  KEY empid (empid),
-  CONSTRAINT fk_event_attendee FOREIGN KEY (event_id) REFERENCES tb_event (event_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_student_attendee FOREIGN KEY (student_id) REFERENCES tbstudinfo (studid) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_employee_attendee FOREIGN KEY (empid) REFERENCES tbempinfo (empid) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `tb_attendees` (
+  `attendee_id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `empid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`attendee_id`),
+  KEY `event_id` (`event_id`),
+  KEY `student_id` (`student_id`),
+  KEY `empid` (`empid`),
+  CONSTRAINT `fk_event_attendee` FOREIGN KEY (`event_id`) REFERENCES `tb_event` (`event_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_student_attendee` FOREIGN KEY (`student_id`) REFERENCES `tbstudinfo` (`studid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_employee_attendee` FOREIGN KEY (`empid`) REFERENCES `tbempinfo` (`empid`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
@@ -230,10 +230,10 @@ DELIMITER $$
 --
 -- Events
 --
-CREATE DEFINER=`root`@`localhost` EVENT update_event_status ON SCHEDULE EVERY 1 SECOND STARTS '2023-11-20 12:35:58' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
-    UPDATE tb_event SET status = 'upcoming' WHERE event_date > CURDATE();
-    UPDATE tb_event SET status = 'ongoing' WHERE event_date = CURDATE();
-    UPDATE tb_event SET status = 'ended' WHERE event_date < CURDATE();
+CREATE DEFINER=`root`@`localhost` EVENT `update_event_status` ON SCHEDULE EVERY 1 SECOND STARTS '2023-11-20 12:35:58' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
+    UPDATE `tb_event` SET `status` = 'upcoming' WHERE `event_date` > CURDATE();
+    UPDATE `tb_event` SET `status` = 'ongoing' WHERE `event_date` = CURDATE();
+    UPDATE `tb_event` SET `status` = 'ended' WHERE `event_date` < CURDATE();
 END$$
 
 DELIMITER ;
