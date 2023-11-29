58cc235ff9174546bfaadbaa24cf6c2ae2ab2dcf
Leo Mikko Uson <usonleomikko@gmail.com>
2023-11-29 08:11:15 +0800

    Refactor session logout code

--- student/logout.php
+++ student/logout.php
@@ -1,5 +1,20 @@
 <?php
 include '../../config/config.php';
-session_destroy(); // Destroy the session
-header("Location: signin.php"); // Redirect to login page
+// Unset all session variables
+$_SESSION = array();
+
+// Delete the session cookie
+if(ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Destroy the session
+session_destroy();
+
+// Redirect to login page
+header("Location: signin.php");
 exit();
