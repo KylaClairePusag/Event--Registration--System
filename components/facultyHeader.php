d712dd9fcbcb9d9d2edcf4c30a70af258cb288d4
Leo Mikko Uson <usonleomikko@gmail.com>
2023-11-30 16:15:11 +0800

    Refactor facultyHeader.php to improve code structure and readability

--- components/facultyHeader.php
+++ components/facultyHeader.php
@@ -1,16 +1,18 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
+if(session_status() == PHP_SESSION_NONE) {
     session_start();
 }
+
+// Include configuration file
 include '../../config/config.php';
 
-// Check if the student is logged in, otherwise redirect to the login page
-if (!isset($_SESSION['emp_email'])) {
+// Redirect to login page if not logged in
+if(!isset($_SESSION['emp_email'])) {
     header("Location: ../signin.php");
     exit();
 }
 
-// Database connection details
+// Database connection details (move this to the config file)
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -21,20 +23,34 @@ try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Prepare and execute the query to get student profile
-    $sql = "SELECT emp_email, role_id, emp_profile,emp_email FROM tbempaccount WHERE emp_email = :emp_email";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(":emp_email", $_SESSION['emp_email']);
-    $stmt->execute();
-    $empData = $stmt->fetch(PDO::FETCH_ASSOC);
+    // Fetch employee account information
+    $sqlEmpAccount = "SELECT empid, emp_profile, emp_email FROM tbempaccount WHERE emp_email = :emp_email";
+    $stmtEmpAccount = $pdo->prepare($sqlEmpAccount);
+    $stmtEmpAccount->bindParam(":emp_email", $_SESSION['emp_email']);
+    $stmtEmpAccount->execute();
+    $empData = $stmtEmpAccount->fetch(PDO::FETCH_ASSOC);
+    $emp_id = $empData['empid'];
     $emp_email = $empData['emp_email'];
-    $empName = $empData['emp_email'];
-    $empPosition = $empData['role_id'];
     $empProfile = $empData['emp_profile'];
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
 
+    // Fetch employee info from tbempinfo based on the empid
+    $sqlEmpInfo = "SELECT * FROM tbempinfo WHERE empid = :empid";
+    $stmtEmpInfo = $pdo->prepare($sqlEmpInfo);
+    $stmtEmpInfo->bindParam(":empid", $emp_id);
+    $stmtEmpInfo->execute();
+    $empInfo = $stmtEmpInfo->fetch(PDO::FETCH_ASSOC);
+    $lastname = $empInfo['lastname'];
+    $firstname = $empInfo['firstname'];
+    $department = $empInfo['department'];
+
+    // Fetch student information from tbstudinfo (you already have this part)
+    $sqlStudInfo = "SELECT * FROM tbstudinfo";
+    $stmtStudInfo = $pdo->prepare($sqlStudInfo);
+    $stmtStudInfo->execute();
+    $studData = $stmtStudInfo->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Connection failed: ".$e->getMessage());
+}
 ?>
 
 <!DOCTYPE html>
@@ -44,28 +60,46 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../styles/header.css" type="text/css">
-    <title>Faculty Dashboard</title>
+    <title>Employee Dashboard</title>
+    <link rel="stylesheet" href="../../styles/style.css">
 </head>
 
 <body>
     <header>
         <nav>
             <div class="logo">
-                <h3>Event</h3>
-                <ul>
-                    <li <?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'class="active"' : ''; ?>>
-                        <a href="./">Overview</a>
-                    </li>
-                    <!-- Add other student-specific menu items as needed -->
-                </ul>
+                <h3><a href="./">Event</a></h3>
+
             </div>
-            <div class="profile">
-                <?php echo htmlspecialchars($emp_email); ?>
-                <?php echo '<img src="../../' . $empProfile . '" alt="emp Profile Image" class="profile"'; ?>
+            <div class="profile dropdown">
+                <span class="profile-email">
+                    <?= htmlspecialchars($firstname); ?>
+                    <?= htmlspecialchars($lastname); ?>
+
+                </span>
+                <div class="profile">
+                    <?php
+                    if(!empty($empProfile)) {
+                        echo '<img src="../../'.$empProfile.'" alt="emp Profile Image" onclick="toggleProfileDropdown()" class="profile-img" >';
+                    } else {
+                        echo '<img src="../../images/alt.png" alt="emp Profile Image" onclick="toggleProfileDropdown()" class="profile-img" >';
+                    }
+                    ?>
+                </div>
+                <div class="profile-dropdown" id="profileDropdown">
+                    <!-- Add profile dropdown items as needed -->
+                    <h5>
+                        <?= htmlspecialchars($emp_email); ?>
+                    </h5>
+
+                    <a href="../logout.php">Logout</a>
+                </div>
             </div>
-            <a href="../logout.php">Logout</a>
         </nav>
     </header>
+
+    <script src="../../script/emp.js"></script>
+    <script src="../../script/script.js"></script>
 </body>
 
 </html>
